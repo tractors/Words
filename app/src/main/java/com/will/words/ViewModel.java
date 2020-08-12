008beb3343a5f41f17c8024cb1520014a5230592
tractors <huijianhua@163.com>
2020-08-13 03:34:48 +0800

    app complete

--- app/src/main/java/com/will/words/ViewModel.java
+++ app/src/main/java/com/will/words/ViewModel.java
@@ -51,4 +51,18 @@ public class ViewModel extends AndroidViewModel {
     }
 
 
+    /**
+     * 入集合和两个角标使集合中元素交换位置
+     * @param list
+     * @param index1
+     * @param index2
+     * @param <E>
+     */
+    public static <E> void swap(List<E> list,int index1,int index2) {
+        //定义第三方变量
+        E e=list.get(index1);
+        //交换值
+        list.set(index1, list.get(index2));
+        list.set(index2, e);
+    }
 }
--- app/src/main/java/com/will/words/WordsFragment.java
+++ app/src/main/java/com/will/words/WordsFragment.java
@@ -3,11 +3,16 @@ package com.will.words;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
@@ -16,6 +21,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -29,6 +36,7 @@ import android.widget.CompoundButton;
 import android.widget.SearchView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -41,9 +49,12 @@ public class WordsFragment extends Fragment {
     private static final String IS_USING_CARD_VIEW = "is_using_card_view";
     private ViewModel viewModel;
     private RecyclerView recyclerView;
-    private MyAdapter myAdapter1,myAdapter2;
+    private MyAdapter myAdapter1, myAdapter2;
     private FloatingActionButton floatingActionButton;
     private LiveData<List<Word>> filteredWords;
+    private List<Word> allWords;
+    private volatile boolean undoAction;
+    private DividerItemDecoration dividerItemDecoration;
 
 
     public WordsFragment() {
@@ -61,34 +72,36 @@ public class WordsFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        viewModel = new ViewModelProvider(requireActivity(),new SavedStateViewModelFactory(requireActivity().getApplication(),requireActivity())).get(ViewModel.class);
+        viewModel = new ViewModelProvider(requireActivity(), new SavedStateViewModelFactory(requireActivity().getApplication(), requireActivity())).get(ViewModel.class);
 
         recyclerView = requireActivity().findViewById(R.id.recycler_view);
         floatingActionButton = requireActivity().findViewById(R.id.floatingActionButton);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
 
-        myAdapter1 = new MyAdapter(false,viewModel);
-        myAdapter2 = new MyAdapter(true,viewModel);
+        myAdapter1 = new MyAdapter(false, viewModel);
+        myAdapter2 = new MyAdapter(true, viewModel);
 
+        dividerItemDecoration = new DividerItemDecoration(requireActivity(),DividerItemDecoration.VERTICAL);
         SharedPreferences preferences = requireActivity().getSharedPreferences(VIEW_TYPE_SHP, Context.MODE_PRIVATE);
-        boolean viewType = preferences.getBoolean(IS_USING_CARD_VIEW,false);
-        if (viewType){
+        boolean viewType = preferences.getBoolean(IS_USING_CARD_VIEW, false);
+        if (viewType) {
             recyclerView.setAdapter(myAdapter2);
         } else {
             recyclerView.setAdapter(myAdapter1);
+            recyclerView.addItemDecoration(dividerItemDecoration);
         }
 
-        recyclerView.setItemAnimator(new DefaultItemAnimator(){
+        recyclerView.setItemAnimator(new DefaultItemAnimator() {
             @Override
             public void onAnimationFinished(@NonNull RecyclerView.ViewHolder viewHolder) {
                 super.onAnimationFinished(viewHolder);
                 LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-                if (layoutManager != null){
+                if (layoutManager != null) {
                     int firstPosition = layoutManager.findFirstVisibleItemPosition();
                     int lastPosition = layoutManager.findLastVisibleItemPosition();
 
-                    for (int i = firstPosition; i <= lastPosition; i++){
+                    for (int i = firstPosition; i <= lastPosition; i++) {
                         MyAdapter.MyViewHolder holder = (MyAdapter.MyViewHolder) recyclerView.findViewHolderForAdapterPosition(i);
                         if (holder != null) {
                             holder.textViewNumber.setText(String.valueOf(i + 1));
@@ -103,9 +116,14 @@ public class WordsFragment extends Fragment {
             public void onChanged(List<Word> words) {
 
                 int temp = myAdapter1.getItemCount();
+                allWords = words;
+                if (temp != words.size()) {
+                    if (temp < words.size() && !undoAction) {
 
-                if (temp != words.size()){
-                    recyclerView.smoothScrollBy(0,-200);
+                        recyclerView.smoothScrollBy(0, -200);
+                    }
+
+                    undoAction = false;
                     myAdapter1.submitList(words);
                     myAdapter2.submitList(words);
                 }
@@ -139,13 +157,12 @@ public class WordsFragment extends Fragment {
 //        });
 
 
-
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.main_menu, menu);
         SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
         searchView.setMaxWidth(1000);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -163,8 +180,8 @@ public class WordsFragment extends Fragment {
                     @Override
                     public void onChanged(List<Word> words) {
                         int temp = myAdapter1.getItemCount();
-
-                        if (temp != words.size()){
+                        allWords = words;
+                        if (temp != words.size()) {
                             myAdapter1.submitList(words);
                             myAdapter2.submitList(words);
                         }
@@ -173,11 +190,84 @@ public class WordsFragment extends Fragment {
                 return true;
             }
         });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.START | ItemTouchHelper.END) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                //通过ID调换位置
+//                Word wordFrom = allWords.get(viewHolder.getAdapterPosition());
+//                Word wordTo = allWords.get(target.getAdapterPosition());
+//                int idTemp = wordFrom.getId();
+//                wordFrom.setId(wordTo.getId());
+//                wordTo.setId(idTemp);
+
+                ViewModel.swap(allWords,viewHolder.getAdapterPosition(),target.getAdapterPosition());
+
+//                viewModel.updateWords(wordFrom,wordTo);
+                myAdapter1.notifyItemMoved(viewHolder.getAdapterPosition(),target.getAdapterPosition());
+                myAdapter2.notifyItemMoved(viewHolder.getAdapterPosition(),target.getAdapterPosition());
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                final Word wordDelete = allWords.get(viewHolder.getAdapterPosition());
+                viewModel.deleteWords(wordDelete);
+                Snackbar.make(requireActivity().findViewById(R.id.words_fragment_view), "删除了一个词汇", Snackbar.LENGTH_SHORT)
+                        .setAction("撤销", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                undoAction = true;
+                                viewModel.insertWords(wordDelete);
+                            }
+                        }).show();
+            }
+
+            //在滑动的时候，画出浅灰色背景和垃圾桶图标，增强删除的视觉效果
+            Drawable icon = ContextCompat.getDrawable(requireActivity(),R.drawable.ic_delete_forever_black_24dp);
+            Drawable background = new ColorDrawable(Color.LTGRAY);
+
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                View itemView = viewHolder.itemView;
+                int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+
+                int iconLeft,iconRight,iconTop,iconBottom;
+                int backTop,backBottom,backLeft,backRight;
+                backTop = itemView.getTop();
+                backBottom = itemView.getBottom();
+                iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+                iconBottom = iconTop + icon.getIntrinsicHeight();
+
+                if (dX > 0){
+                    backLeft = itemView.getLeft();
+                    backRight = itemView.getLeft() + (int) dX;
+                    background.setBounds(backLeft,backTop,backRight,backBottom);
+                    iconLeft = itemView.getLeft() + iconMargin;
+                    iconRight = iconLeft + icon.getIntrinsicWidth();
+                    icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+                } else if (dY < 0){
+                    backRight = itemView.getRight();
+                    backLeft = itemView.getRight() + (int) dX;
+                    background.setBounds(backLeft,backTop,backRight,backBottom);
+                    iconRight = itemView.getRight() - iconMargin;
+                    iconLeft = iconRight - icon.getIntrinsicWidth();
+                    icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+                } else {
+                    background.setBounds(0,0,0,0);
+                    icon.setBounds(0,0,0,0);
+                }
+
+                background.draw(c);
+                icon.draw(c);
+            }
+        }).attachToRecyclerView(recyclerView);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.clearData:
                 AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
                 builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
@@ -198,14 +288,16 @@ public class WordsFragment extends Fragment {
             case R.id.switchViewType:
                 SharedPreferences preferences = requireActivity().getSharedPreferences(VIEW_TYPE_SHP, Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = preferences.edit();
-                boolean viewType = preferences.getBoolean(IS_USING_CARD_VIEW,false);
+                boolean viewType = preferences.getBoolean(IS_USING_CARD_VIEW, false);
 
-                if (viewType){
+                if (viewType) {
                     recyclerView.setAdapter(myAdapter1);
-                    editor.putBoolean(IS_USING_CARD_VIEW,false);
+                    recyclerView.addItemDecoration(dividerItemDecoration);
+                    editor.putBoolean(IS_USING_CARD_VIEW, false);
                 } else {
                     recyclerView.setAdapter(myAdapter2);
-                    editor.putBoolean(IS_USING_CARD_VIEW,true);
+                    editor.putBoolean(IS_USING_CARD_VIEW, true);
+                    recyclerView.removeItemDecoration(dividerItemDecoration);
                 }
 
                 editor.apply();
